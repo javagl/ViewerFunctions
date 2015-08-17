2c190e2505df2d097d0a58eaceff3cd0bdb17fcd
Marco Hutter <javagl@javagl.de>
2015-08-17 21:06:23 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ViewerFunctions
+
 A simple function plotter
+
+![ViewerFunctionsScreenshot01.png](/screenshots/ViewerFunctionsScreenshot01.png)
+
+The [FunctionPanel](https://github.com/javagl/ViewerCells/blob/master/src/main/java/de/javagl/viewer/functions/FunctionPanel.java) 
+is a [Viewer](https://github.com/javagl/Viewer) for `DoubleFunction`s. 
+It allows zooming and translating the view, and paints labeled axes
+and legends. 
+
+The basic usage is demonstrated in the 
+[ViewerFunctionsTest](https://github.com/javagl/ViewerCells/blob/master/src/test/java/de/javagl/viewer/functions/test/ViewerFunctionsTest.java)
+class.
+
+Currently, the function plotter does not allow much configuration,
+as the focus was on the ease of use: A few lines of codes are
+sufficient to create a simple plot:
+
+![ViewerFunctionsScreenshot02.png](/screenshots/ViewerFunctionsScreenshot02.png)
+
+
---
+++ src/main/java/de/javagl/viewer/functions/Axes.java
@@ -0,0 +1,175 @@
+/*
+ * www.javagl.de - Viewer - Functions
+ *
+ * Copyright (c) 2013-2015 Marco Hutter - http://www.javagl.de
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package de.javagl.viewer.functions;
+
+/**
+ * Utility methods related to computations of ticks on axes
+ */
+class Axes
+{
+    /**
+     * The default values to snap to
+     */
+    private static final double DEFAULT_SNAP_VALUES[] = 
+        new double[]{ 1.0, 2.0, 5.0 };
+    
+    /**
+     * Returns a format string that can be used in <code>String#format</code> 
+     * to format values of the given order. The exact meaning of this is 
+     * intentionally left unspecified, but for numbers that are "reasonable" 
+     * to be displayed as decimal numbers (without scientific notation),
+     * this function will return a format with the "appropriate" number of
+     * decimal digits in order to format axis labels.  
+     * 
+     * @param order The order
+     * @return The format string
+     */
+    static String formatStringFor(double order)
+    {
+        if (order < 1e-100 || !Double.isFinite(order))
+        {
+            return "%f";
+        }
+        double exponent = Math.floor(Math.log10(order));
+        int digits = (int)Math.abs(exponent);
+        if (order >= 1.0)
+        {
+            digits = 0;
+        }
+        String result = "%."+digits+"f";
+        return result;
+    }
+    
+    /**
+     * Computes the coordinates of ticks for an axis with the given parameters
+     * 
+     * @param worldMin The minimum world coordinate on the axis
+     * @param worldMax The maximum world coordinate on the axis
+     * @param worldTickDistance The distance that two ticks
+     * should have in world coordinates
+     * @return The world coordinates for the ticks
+     */
+    static double[] computeWorldTicks(
+        double worldMin, double worldMax, double worldTickDistance)
+    {
+        long nMin = (long) Math.ceil(worldMin / worldTickDistance);
+        long nMax = (long) Math.floor(worldMax / worldTickDistance);
+        int n = (int) (nMax - nMin + 1);
+        double worldTicks[] = new double[n];
+        for (long i = nMin; i <= nMax; i++)
+        {
+            double w = i * worldTickDistance;
+            worldTicks[(int) (i - nMin)] = w;
+        }
+        return worldTicks;
+    }
+    
+    /**
+     * Computes the smallest value greater than or equal to the given value, 
+     * and that is of the form d*(10^x), where d is any of {1.0, 2.0, 5.0} 
+     * 
+     * @param value The value
+     * @return The snapped value
+     */
+    static double computeSnappedUpValue(double value)
+    {
+        return computeSnappedUpValue(value, DEFAULT_SNAP_VALUES);
+    }
+    
+    /**
+     * Computes the smallest value greater than or equal to the given value, 
+     * and that is of the form d*(10^x), where d is any of the given divisors 
+     * 
+     * @param value The value
+     * @param divisors The divisors
+     * @return The snapped value
+     */
+    static double computeSnappedUpValue(double value, double divisors[])
+    {
+        double exponent = Math.floor(Math.log10(value));
+        double scaling = Math.pow(10, exponent);
+        double scaledValue = value / scaling;
+        double bestDivisor = 10.0 * divisors[0];
+        for (int i = 0; i < divisors.length; i++)
+        {
+            double divisor = divisors[i];
+            if (scaledValue <= divisor)
+            {
+                bestDivisor = divisor;
+                break;
+            }
+        }
+        double snappedValue = bestDivisor * scaling;
+        return snappedValue;
+    }
+    
+    /**
+     * Computes the greatest value smaller than or equal to the given value, 
+     * and that is of the form d*(10^x), where d is any of {1.0, 2.0, 5.0} 
+     * 
+     * @param value The value
+     * @return The snapped value
+     */
+    static double computeSnappedDownValue(double value)
+    {
+        return computeSnappedDownValue(value, DEFAULT_SNAP_VALUES);
+    }
+    
+    /**
+     * Computes the smallest value greater than or equal to the given value, 
+     * and that is of the form d*(10^x), where d is any of the given divisors 
+     * 
+     * @param value The value
+     * @param divisors The divisors
+     * @return The snapped value
+     */
+    static double computeSnappedDownValue(double value, double divisors[])
+    {
+        double exponent = Math.floor(Math.log10(value));
+        double scaling = Math.pow(10, exponent);
+        double scaledValue = value / scaling;
+        double bestDivisor = divisors[0];
+        for (int i = 0; i < divisors.length; i++)
+        {
+            double divisor = divisors[i];
+            if (scaledValue >= divisor)
+            {
+                bestDivisor = divisor;
+            }
+        }
+        double snappedValue = bestDivisor * scaling;
+        return snappedValue;
+    }
+    
+    /**
+     * Private constructor to prevent instantiation
+     */
+    private Axes()
+    {
+        // Private constructor to prevent instantiation
+    }
+}
